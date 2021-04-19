6ba8ec37ec1afd29cd281889e311c6c382f1b055
mac12 <mac12@Macbooks-MacBook-Pro.local>
2021-04-19 22:04:54 +0300

    subolders commented

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,7 +77,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-  generate: {
-    subFolders: false
-  }
+  // generate: {
+  //   subFolders: false
+  // }
 }
