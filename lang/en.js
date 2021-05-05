0ff97fe8a4187984b44fedb80022663185199530
mac13 <mac13@Macbooks-MacBook-Pro.local>
2021-05-05 12:11:02 +0300

    generate deploaybles

--- lang/en.js
+++ lang/en.js
@@ -35,5 +35,39 @@ export default {
     "OLD_PRICE": "Price: {oldPrice} ({percentageDiscount}% off)",
     "OR_PAY_LATER": "or Pay Later {br} {paymentAmount} {numberOfPayments}",
     "X_NUMBER_OF_PAYMENTS": "x{numberOfPayments} payments",
-    "CLOSE": "Close"
+    "CLOSE": "Close",
+    "FOOTER": {
+        CORPORATION_INFORMATION_COUNT: '4',
+        CORPORATION_INFORMATION_1: '4Team Corporation',
+        CORPORATION_INFORMATION_2: '5645 Coral Ridge Dr #211',
+        CORPORATION_INFORMATION_3: 'Coral Springs',
+        CORPORATION_INFORMATION_4: 'FL 33076, USA',
+        CONTACTS_COUNT: '5',
+        CONTACTS_NAME_1: 'USA',
+        CONTACTS_NUMBER_1: '+1 (954) 796-8161',
+        CONTACTS_NAME_2: 'UK',
+        CONTACTS_NUMBER_2: '+1 (954) 796-8161',
+        CONTACTS_NAME_3: 'Canada',
+        CONTACTS_NUMBER_3: '+1 647 477-3340',
+        CONTACTS_NAME_4: 'France',
+        CONTACTS_NUMBER_4: '+33 1 86 26 42',
+        CONTACTS_NAME_5: 'Skype',
+        CONTACTS_NUMBER_5: 'teamcorporation',
+        CONTACTS_NAME_6: 'Fax',
+        CONTACTS_NUMBER_6: '+1 (954) 780-3795',
+        BUTTONS_COUNT: '3',
+        BUTTON_NAME_1: 'About Us',
+        BUTTON_LINK_1: '/about',
+        BUTTON_NAME_2: 'Privacy',
+        BUTTON_LINK_2: '/privacy',
+        BUTTON_NAME_3: 'Terms',
+        BUTTON_LINK_3: '/terms',
+        INPUT_LABEL: 'Keep me posted on the latest news',
+        INPUT_PLACEHOLDER: 'Type your Email address here',
+        FOOTER_BOTTOM: {
+            COPYRIGHT: '4Team Corporation All rights reserved',
+            MAIN_TEXT:
+                'Microsoft and the Office logo are registered trademarks of Microsoft Corporation in the United States and/or other countries. Apple and the iPhone, iCloud, iTunes logos are registered trademarks of Apple Inc. In the United States and/or other countries. Google and the Google Calendar, Google Contacts, Google Tasks logos are registered trademarks of Google LLC. In the United States and/or other countries.',
+        }
+    }
 }
