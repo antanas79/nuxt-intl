c72a79f8198ee62b0ad1074a95287f4df3b4e751
mac13 <mac13@Macbooks-MacBook-Pro.local>
2021-05-12 18:27:26 +0300

    style changes

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,15 +1,15 @@
-// import Vue from 'vue'
-// import Vuetify from 'vuetify/lib'
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
 
-// export default new Vuetify({
-//   breakpoint: {
-//     thresholds: {
-//       // xs: 340,
-//       sm: 576,
-//       md: 768,
-//       lg: 992,
-//       xl: 1200
-//     },
-//     scrollBarWidth: 24,
-//   },
-// })
+export default new Vuetify({
+  breakpoint: {
+    thresholds: {
+      // xs: 340,
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200
+    },
+    scrollBarWidth: 24,
+  },
+})
