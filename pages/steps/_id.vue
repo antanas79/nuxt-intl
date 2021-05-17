0843259bc8a12029a5e8485768d92867a57ea12f
mac13 <mac13@Macbooks-MacBook-Pro.local>
2021-05-17 11:56:56 +0300

    changed styles

--- pages/steps/_id.vue
+++ pages/steps/_id.vue
@@ -37,6 +37,21 @@
                     >
                     </PricingCard>
                   </div>
+                  <div>
+                    <v-sheet class="mx-auto" elevation="8" max-width="800">
+                      <v-slide-group v-model="model" class="pa-4" multiple show-arrows>
+                        <v-slide-item v-for="n in 15" :key="n" v-slot="{ active, toggle }">
+                          <v-card :color="active ? 'primary' : 'grey lighten-1'" class="ma-4" height="200" width="100" @click="toggle">
+                            <v-row class="fill-height" align="center" justify="center">
+                              <v-scale-transition>
+                                <v-icon v-if="active" color="white" size="48" v-text="'mdi-close-circle-outline'"></v-icon>
+                              </v-scale-transition>
+                            </v-row>
+                          </v-card>
+                        </v-slide-item>
+                      </v-slide-group>
+                    </v-sheet>
+                  </div>
                 </div>
               </div>
             </div>
@@ -58,6 +73,7 @@ export default {
     return {
       payload: null,
       isLoaded: false,
+      model: [],
     }
   },
   computed: mapState({
